c07a7ce0b2427ddbc14356cf387d933e9b8f5583
DPTPhatUS <tuanphat51sp@gmail.com>
2026-07-12 00:31:16 +0700

    correct device id

--- train.py
+++ train.py
@@ -28,7 +28,7 @@ def setup_distributed():
         rank = int(os.environ["RANK"])
         world_size = int(os.environ["WORLD_SIZE"])
         local_rank = int(os.environ["LOCAL_RANK"])
-        dist.init_process_group(backend="nccl")
+        dist.init_process_group(backend="nccl", device_id=torch.device(f"cuda:{local_rank}"))
         torch.cuda.set_device(local_rank)
         return rank, world_size, local_rank, True
     else:
